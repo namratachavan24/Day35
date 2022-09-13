874babb6d30e124c182fc8ee66dbd02bc6448dae
Namrata Chavan <109748579+namratachavan24@users.noreply.github.com>
2022-09-13 16:27:41 +0530

    UC-6 Added

--- ProductReviewManagement.cs
+++ ProductReviewManagement.cs
@@ -11,11 +11,8 @@ namespace LINQProductReview
         public static void RetriveTo3ReviewBasedOnTheHighestRating(List<ProductReview> list)
         {
             Console.WriteLine("\nSorted Order");
-            //here we are using query syntax
-            //here we are retriving the product review based on the higest rating for that we are sortin gthe list in desending oredr
             List<ProductReview> sortedList = (from product in list orderby product.Rating descending select product).ToList();
             Program.IterateOverProductReview(sortedList);
-            //after sortig we are taking only top 3 so thas why we are usigng take method for the sroted order
             Console.WriteLine("\nRetriveint To 3Review Based On The Highest Rating");
             List<ProductReview> top4res = sortedList.Take(3).ToList();
             Program.IterateOverProductReview(top4res);
@@ -26,16 +23,11 @@ namespace LINQProductReview
         public static void RetriveBasedOnProductIdAndRatig(List<ProductReview> list)
         {
             Console.WriteLine("\nretriving the productid and rating");
-            //this is using the query synatx
-            //var res = list.Where(p => p.Rating > 3 && (p.ProductId == 1 || p.ProductId == 4 || p.ProductId == 9));
-            //this for the method syntax
             var res1 = (from p in list where p.Rating > 3 && (p.ProductId.Equals(1) || p.ProductId.Equals(4) || p.ProductId.Equals(9)) select p).ToList();
             Program.IterateOverProductReview(res1);
         }
         public static void CountEachProductID(List<ProductReview> list)
         {//uc4
-            //here groupby duplicate values are removed and we are slecting the productid as key
-            //and aftre that we are counting how many times ecah porductid is repeating
             Console.WriteLine("\nretriving the count of the product id");
             var CountOfProductId = (list.GroupBy(p => p.ProductId).Select(product => new { productId = product.Key, ncount = product.Count() })).ToList();
             foreach (var product in CountOfProductId)
@@ -52,6 +44,13 @@ namespace LINQProductReview
             {
                 Console.WriteLine("ProductId:{0} Review: {1}", product.productID, product.review);
             }
+        }
+        //uc6
+        public static void SkipTop5DataRetriveRemaining(List<ProductReview> list)
+        {
+            Console.WriteLine("\nSkip the Top 5 Records display remaing records");
+            List<ProductReview> AfterskipRecords = list.Skip(5).ToList();
+            Program.IterateOverProductReview(AfterskipRecords);
         }
     }
 }
